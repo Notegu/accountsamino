f91a01cd7b554b50cde8099cbe259a5442dd76c3
Notegu <96369209+Notegu@users.noreply.github.com>
2021-12-24 18:30:20 +0300

    Update new.py

--- new.py
+++ new.py
@@ -2,16 +2,16 @@ key='8b6682d7-d9b6-40d8-9897-0c35d706940a'
 
 app_name="accountsanino"
 
-device="17610295062127B93691DEC388CFF0675DF245E1F6418C92AA2C90619D428DAF54735B128B616B96AC"
+deviceid="32D6A082DA37BED2C26127AF734AC39DFE5096CB5A48BA4EFD7CBC0CED86AF1F60D8CD5E398885751A"
 
-emaill="hmstfymadz@gmail.com"
+emaill="hmstfymadZ@gmail.com"
 
 passwordd="DARKMewa1990"
 
-custompwd=""
+custompwd="fakebosh"
 
 private=""
 
 chatlink="http://aminoapps.com/p/rkcrfc"
 
-nickname="fjxnenf"
+nickname="bosh"
